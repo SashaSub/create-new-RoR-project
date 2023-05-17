1866b830d59585e013883acae39096bad70dd56f
SashaSub <ssuboch@gMAIL.com>
2023-05-17 17:41:32 -0400

    Added three projects to the page Projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/projects'
+  get 'pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "home",   to:"pages#home",    as:"home"
+  get "projects",   to:"pages#projects",    as:"projects"
+  get "contact",   to:"pages#contact",    as:"contact"
   # Defines the root path route ("/")
   # root "articles#index"
+  root "pages#home"
 end
